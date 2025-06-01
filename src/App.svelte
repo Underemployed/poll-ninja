27ee70c5028ad91e270738749626c472badf3387
Underemployed <underemployed871@gmail.com>
2025-06-01 11:55:32 +0530

    conditionals and components

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,8 @@
 <script>
   import { handle_promise } from "svelte/internal";
 
+  import Modal from "./Modal.svelte"
+
 let blogs = [
 	{
 		id: 1,
@@ -26,14 +28,28 @@ const deleteBlog = (id) => {
 	console.log(id)
 	blogs = blogs.filter((blog) => blog.id !== id)
 }
+$: num = blogs.length;
 
 </script>
-
+<Modal/>
 <main>
+	{#if num === 3}
+	<p>3</p>
+	{:else if num === 2}
+	 <p>2</p>
+	{:else if num === 1}
+	 <p>1</p>
+	{:else}
+	<p>dead</p>
+	{/if}
+
  {#each blogs as blog (blog.id)}
   <div>
 	{blog.id}
 	<p>{blog.title}</p>
+	{#if blog.likes === 20}
+	<p>omg	</p>
+	{/if}
 	<button on:click={() => {deleteBlog(blog.id)}}>delete blog</button>
 
   </div>
---
+++ src/Modal.svelte
@@ -0,0 +1,14 @@
+
+<script>
+
+</script>
+
+<div class="backdrop">
+    <div class="modal">
+        <p>Signup for more offers</p>
+    </div>
+</div>
+
+<style>
+    
+</style>
